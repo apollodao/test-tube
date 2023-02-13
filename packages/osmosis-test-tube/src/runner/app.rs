1cf1abe9368c790c3b293f596b4d884709ff0d5e
Sturdy <91910406+apollo-sturdy@users.noreply.github.com>
2023-02-14 00:41:20 +0100

    feat: add fn on app for forcewithdraw whitelist

--- packages/osmosis-test-tube/src/runner/app.rs
+++ packages/osmosis-test-tube/src/runner/app.rs
@@ -7,6 +7,7 @@ use serde::de::DeserializeOwned;
 use test_tube::account::SigningAccount;
 
 use test_tube::bindings::AddSuperfluidLPShare;
+use test_tube::bindings::WhitelistAddressForForceUnlock;
 use test_tube::runner::result::{RunnerExecuteResult, RunnerResult};
 use test_tube::runner::Runner;
 use test_tube::{redefine_as_go_string, BaseApp};
@@ -64,6 +65,19 @@ impl OsmosisTestApp {
     /// Get the first validator address
     pub fn get_first_validator_address(&self) -> RunnerResult<String> {
         self.inner.get_first_validator_address()
+    }
+
+    /// Get the first validator signing account
+    pub fn get_first_validator_signing_account(&self) -> RunnerResult<SigningAccount> {
+        self.inner.get_first_validator_signing_account()
+    }
+
+    /// Whitelist an address for force unlock
+    pub fn whitelist_address_for_force_unlock(&self, address: &str) {
+        redefine_as_go_string!(address);
+        unsafe {
+            WhitelistAddressForForceUnlock(self.inner.id(), address);
+        }
     }
 
     /// Increase the time of the blockchain by the given number of seconds.
