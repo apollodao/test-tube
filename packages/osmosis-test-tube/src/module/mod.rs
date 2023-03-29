ae6fc70ff6ccd56baec817c39fc9437067880061
Sturdy <91910406+apollo-sturdy@users.noreply.github.com>
2023-03-29 17:36:47 +0200

    feat: Move bank and wasm modules to test-tube

--- packages/osmosis-test-tube/src/module/mod.rs
+++ packages/osmosis-test-tube/src/module/mod.rs
@@ -1,9 +1,9 @@
-mod bank;
 mod gamm;
 mod tokenfactory;
-mod wasm;
 
 pub use test_tube::macros;
+pub use test_tube::module::bank;
+pub use test_tube::module::wasm;
 pub use test_tube::module::Module;
 
 pub use bank::Bank;
